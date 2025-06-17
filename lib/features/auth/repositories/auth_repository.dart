ae7342bcadfc8d3922c84cd2f6ed44151b2ab6f0
Glound <glound.app@gmail.com>
2025-06-17 17:16:07 +0900

    auth_repositoryの変更

--- lib/features/auth/repositories/auth_repository.dart
+++ lib/features/auth/repositories/auth_repository.dart
@@ -1,22 +1,29 @@
 // auth_repository.dart
 // Firebase Authenticationとの実際の通信処理を担当するクラス
 import "dart:developer";
+import "package:cloud_firestore/cloud_firestore.dart";
 import "package:firebase_auth/firebase_auth.dart";
 import "package:google_sign_in/google_sign_in.dart";
+import "package:madoi/common/models/user_model.dart";
 
 class AuthRepository {
   final FirebaseAuth _auth;
   final GoogleSignIn _googleSignIn;
+  final FirebaseFirestore _firestore;
 
-  AuthRepository({FirebaseAuth? auth, GoogleSignIn? googleSignIn})
-    : _auth = auth ?? FirebaseAuth.instance,
-      _googleSignIn = googleSignIn ?? GoogleSignIn();
+  AuthRepository({
+    FirebaseAuth? auth,
+    GoogleSignIn? googleSignIn,
+    FirebaseFirestore? firestore,
+  }) : _auth = auth ?? FirebaseAuth.instance,
+       _googleSignIn = googleSignIn ?? GoogleSignIn(),
+       _firestore = firestore ?? FirebaseFirestore.instance;
 
   // ログイン状態を監視するStream
   Stream<User?> get authStateChanges => _auth.authStateChanges();
 
   // Googleでサインイン
-  Future<UserCredential> signInWithGoogle() async {
+  Future<void> signInWithGoogle() async {
     try {
       // Googleサインインのポップアップを表示し、アカウントを選択させる
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
@@ -36,7 +43,32 @@ class AuthRepository {
       );
 
       // Firebaseにサインイン
-      return await _auth.signInWithCredential(credential);
+      final UserCredential userCredential = await _auth.signInWithCredential(
+        credential,
+      );
+
+      // 初回ログインかどうかの判定
+      if (userCredential.user != null) {
+        // usersコレクションにドキュメントが存在するかの確認
+        var userDoc = await _firestore
+            .collection("users")
+            .doc(userCredential.user!.uid)
+            .get();
+
+        // ドキュメントが存在しない場合（=初回ログイン）のみ書き込み
+        if (!userDoc.exists) {
+          // 保存するユーザーモデルを作成
+          var user = UserModel(
+            uid: userCredential.user!.uid,
+            name: userCredential.user!.displayName ?? "名無しさん",
+            email: userCredential.user!.email ?? "",
+            profilePic: userCredential.user!.photoURL ?? "",
+          );
+
+          // Firestoreの"users"コレクションにユーザー情報を保存
+          await _firestore.collection("users").doc(user.uid).set(user.toMap());
+        }
+      }
     } catch (e) {
       // エラー処理
       log("Googleサインインエラー: ${e.toString()}");
